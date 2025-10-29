a6c7b3da50892c2c476757e5c0eac0854f75ed85
corbinlazarone <corbinlazarone1@gmail.com>
2025-10-29 16:49:41 -0500

    feat: more handler skeltons

--- server/cmd/api/handlers.go
+++ server/cmd/api/handlers.go
@@ -9,10 +9,22 @@ func (app *application) health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "cmovie server is running!")
 }
 
+// NOTE: public
 func (app *application) AllReviews() {
-
 }
 
-func (app *application) SingleReview() {
+// NOTE:  public
+func (app *application) SingleReview(w http.ResponseWriter, r *http.Request) {
+}
 
+// NOTE: admin
+func (app *application) CreateReview() {
+}
+
+// NOTE: admin
+func (app *application) EditReview() {
+}
+
+// NOTE: admin
+func (app *application) DeleteReview() {
 }
